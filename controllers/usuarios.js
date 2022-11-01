2640226da0a1bf114733583ee2efab84a0b33526
Jesus Lerdo Atilano <isc19350300@gmail.com>
2022-10-31 20:32:10 -0600

    first commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,29 @@
+const {request, response} = require("express")
+const pool = require("../db/connection")
+
+const getUsers = async (req = request, res = response) => {
+    let conn;
+
+    try {
+        conn = await pool.getConnection()//realizamos la conexion
+
+        //Generamos la consulta
+        const users = await conn.query("SELECT * FROM Usuarios", (error) => { if (error) throw error })
+
+        if (!users){// En caso de no haber registros lo informamos
+            res.status(404).json({msg: "No existen usuarios registrados"})
+            return
+        }
+
+        res.json({users})//se manda la lista de usuarios
+    } catch (error){
+        console.log(error)
+        res.status(500).json({msg: error})//Informamos el error
+
+    }finally {
+        if (conn) conn.end()//Termina la conexion
+
+    }
+}
+
+module.exports = {getUsers}
